323fab44119d0ed66a7d800097b28a38760b1f01
Derek Araujo <derekaraujo19@gmail.com>
2022-09-12 13:42:48 -0400

    commit tree 1950bc0e4128a9ad163128eb552882807e1f0aa6

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,40 @@
+# class Song
+#   @@count = 0
+#   @@genres = []
+#   @@artists = []
+#   attr_accessor :name, :artist, :genre
+#   def initialize(name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     @@count += 1
+#     @@genres << @genre
+#     @@artists << @artist
+#   end
+
+#   def self.count
+#     @@count
+#   end
+
+#   def self.genres
+#     @@genres.uniq
+#   end
+
+#   def self.artists
+#     @@artists.uniq
+#   end
+
+#   def self.genre_count
+#     @@genres.tally
+#   end
+
+#   def self.artist_count
+#     @@artists.tally
+#   end
+
+# end
+
+
 class Song
   @@count = 0
   @@genres = []
@@ -12,24 +49,23 @@ class Song
     @@artists << @artist
   end
 
-  def self.count
+  def Song.count
     @@count
   end
 
-  def self.genres
+  def Song.genres
     @@genres.uniq
   end
 
-  def self.artists
+  def Song.artists
     @@artists.uniq
   end
 
-  def self.genre_count
+  def Song.genre_count
     @@genres.tally
   end
 
-  def self.artist_count
+  def Song.artist_count
     @@artists.tally
   end
-
 end
